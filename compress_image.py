d4573ef2f5da66f7535448782e121a088fd197c7
Yash Mistry <61091300+m-yash@users.noreply.github.com>
2023-12-19 03:21:42 -0500

    image compress using pillow library

--- compress_image.py
+++ compress_image.py
@@ -1,7 +1,10 @@
 import PIL
 from tkinter.filedialog import *
 from PIL import Image
+
 file_loc=askopenfilenames()
 img = Image.open(file_loc[0])
+
+# adjust quality value as needed
 img.save("Compressed.jpg", "JPEG", optimize = True, quality = 10)
 print("Image is Compressed")
